277e8f9ce50756a4dea2c8da92d36ce9b4d53cb3
Ilya <Nolanz0r@yandex.ru>
2021-05-05 11:17:40 +0300

    App init

--- react-gallery/src/components/Paper/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import classes from "./Paper.module.scss";
-
-const Paper = ({ children }) => {
-  return <div className={classes.paper}>{children}</div>;
-};
-
-export default Paper;
--- react-gallery/src/utils/randomNumbersArray.js
+++
@@ -1,3 +0,0 @@
-export const getArrayOfRandomNumbers = (length) => {
-  return Array.from({ length: 5 }, () => Math.floor(Math.random() * length));
-};
